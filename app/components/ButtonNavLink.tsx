c725fab23af62c97dc1aeaca4f3eae240d866e01
Seth Davis <sethdavis512@gmail.com>
2024-12-07 00:17:21 -0600

    Update button nav link styles

--- app/components/ButtonNavLink.tsx
+++ app/components/ButtonNavLink.tsx
@@ -1,19 +1,34 @@
 import type { VariantProps } from "cva";
-import { cx } from "cva.config";
+import { cva, cx } from "cva.config";
 import { PropsWithChildren } from "react";
 import { LinkProps, NavLink } from "react-router";
-import { buttonVariants } from "./Button";
+
+export const buttonNavLinkVariants = cva({
+    base: [
+        "rounded-xl text-white",
+        "bg-zinc-500 dark:bg-zinc-900 dark:hover:bg-zinc-600 hover:bg-zinc-600",
+        "border-zinc-700 dark:border-zinc-800",
+        "focus:ring-zinc-300 dark:focus:ring-zinc-800",
+        "px-3.5 py-2",
+    ],
+    variants: {
+        active: {
+            true: [
+                "bg-primary-500 dark:bg-primary-900 dark:hover:bg-primary-600 hover:bg-primary-600",
+                "border-primary-700 dark:border-primary-800",
+                "focus:ring-primary-300 dark:focus:ring-primary-800",
+            ],
+        },
+    },
+});
 
 export interface ButtonNavLinkProps
-    extends Omit<LinkProps, "color">,
-        VariantProps<typeof buttonVariants> {}
+    extends LinkProps,
+        VariantProps<typeof buttonNavLinkVariants> {}
 
 export function ButtonNavLink({
     children,
-    color,
     className,
-    variant,
-    size,
     to,
 }: PropsWithChildren<ButtonNavLinkProps>) {
     return (
@@ -22,11 +37,9 @@ export function ButtonNavLink({
             className={({ isActive }) =>
                 cx(
                     "inline-block",
-                    buttonVariants({
+                    buttonNavLinkVariants({
                         className,
-                        color: isActive ? "secondary" : color,
-                        size,
-                        variant,
+                        active: isActive,
                     }),
                 )
             }
