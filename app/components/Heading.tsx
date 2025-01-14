d4c7de75acf8dc7d4debe98536562f5fbc4a1cc2
Seth Davis <sethdavis512@gmail.com>
2025-01-14 11:21:45 -0600

    Rework dashboard. Add combo box component.

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -7,12 +7,12 @@ export const headingVariants = cva({
     base: "text-zinc-800 dark:text-white font-bold",
     variants: {
         as: {
-            h1: "text-4xl",
-            h2: "text-3xl",
-            h3: "text-2xl",
-            h4: "text-xl",
-            h5: "text-lg",
-            h6: "text-base",
+            h1: "text-5xl",
+            h2: "text-4xl",
+            h3: "text-3xl",
+            h4: "text-2xl",
+            h5: "text-xl",
+            h6: "text-lg",
         },
     },
     defaultVariants: {
--- app/routes/dashboard/index.tsx
+++ app/routes/dashboard/index.tsx
@@ -1,30 +1,16 @@
-import { Outlet, useFetcher, useNavigation } from "react-router";
+import { Form, Link, Outlet, useNavigation } from "react-router";
 import invariant from "tiny-invariant";
-import {
-    FileIcon,
-    FileQuestion,
-    ListIcon,
-    LoaderPinwheelIcon,
-    MessagesSquareIcon,
-    PencilIcon,
-    SparkleIcon,
-    TrashIcon,
-} from "lucide-react";
-import { PortableText } from "@portabletext/react";
 
-import { Banner } from "~/components/Banner";
-import { Button } from "~/components/Button";
-import { ButtonLink } from "~/components/ButtonLink";
 import { Card } from "~/components/Card";
+import { ComboBox } from "~/components/form/ComboBox";
 import { getThreadsByUserId } from "~/models/thread.server";
 import { getUser } from "~/utils/auth.server";
 import { getUsersAssistants } from "~/models/assistant.server";
 import { Heading } from "~/components/Heading";
-import { HorizontalRule } from "~/components/HorizontalRule";
-import { Paths } from "~/utils/paths";
-import { ellipsisify } from "~/utils/string";
 import { Route } from "../dashboard/+types";
-import { p } from "node_modules/@react-router/dev/dist/routes-DHIOx0R9";
+import { TextFormField } from "~/components/form/TextFormField";
+import { Field, Label } from "@headlessui/react";
+import { LoaderPinwheelIcon } from "lucide-react";
 
 export async function loader({ request }: Route.LoaderArgs) {
     const user = await getUser(request);
@@ -53,246 +39,65 @@ export async function loader({ request }: Route.LoaderArgs) {
 
 export default function Dashboard({ loaderData }: Route.ComponentProps) {
     const { assistants, threads } = loaderData;
+    const timeOfDayGreeting = `Good morning`;
     const navigation = useNavigation();
 
-    const threadFetcher = useFetcher();
-    const getCreateThreadChatIcon = (assistantId: string) =>
-        threadFetcher.state !== "idle" &&
-        threadFetcher.formAction?.includes(assistantId) ? (
-            <LoaderPinwheelIcon className="h-4 w-4 animate-spin" />
-        ) : (
-            <SparkleIcon className="h-4 w-4" />
-        );
-
-    const getDeleteAssistantIcon = (assistantId: string) =>
-        threadFetcher.state !== "idle" &&
-        threadFetcher.formMethod === "DELETE" &&
-        threadFetcher.formData?.get("assistantId") === assistantId ? (
-            <LoaderPinwheelIcon className="h-4 w-4 animate-spin" />
-        ) : (
-            <TrashIcon className="h-4 w-4" />
-        );
-
-    const deleteThreadFetcher = useFetcher();
-    const getDeleteChatIcon = (threadId: string) =>
-        deleteThreadFetcher.state !== "idle" &&
-        deleteThreadFetcher.formAction?.includes(threadId) ? (
-            <LoaderPinwheelIcon className="h-4 w-4 animate-spin" />
-        ) : (
-            <TrashIcon className="h-4 w-4" />
-        );
-
-    const agents = [] as any;
+    const getThreadIsLoading = (threadId: string) =>
+        navigation && navigation.location?.pathname.includes(threadId);
 
     return (
         <>
-            <div className="px-4">
-                <div className="mb-4 flex items-center gap-4">
-                    <Heading>Assistants</Heading>
-                    <ButtonLink
-                        className="inline-flex items-center gap-2"
-                        to={Paths.CREATE_ASSISTANT}
-                        color="secondary"
-                        iconBefore={<PencilIcon className="h-4 w-4" />}
-                    >
-                        Create assistant
-                    </ButtonLink>
+            <div className="grid h-full grid-cols-12 grid-rows-[1fr_auto] gap-4">
+                <div className="col-span-12 row-span-1 flex w-full flex-1 flex-col">
+                    <div className="flex h-full flex-col items-center justify-center gap-8">
+                        <Heading>{`${timeOfDayGreeting}, ${loaderData.user.profile?.firstName}. How can I help?`}</Heading>
+                        <Form
+                            method="POST"
+                            className="flex min-w-[768px] flex-col gap-4 rounded-xl border border-zinc-300 bg-zinc-200 p-4 dark:border-zinc-600 dark:bg-zinc-800"
+                        >
+                            <TextFormField
+                                id="prompt"
+                                name="prompt"
+                                helperText="What would you like to chat about?"
+                                label="Prompt"
+                                className="min-h-11 w-full"
+                            />
+                            <Field>
+                                <Label>Assistant</Label>
+                                <ComboBox
+                                    id="assistantId"
+                                    name="assistantId"
+                                    options={assistants.map((assistant) => ({
+                                        id: assistant.id,
+                                        label: assistant.name as string,
+                                    }))}
+                                />
+                            </Field>
+                        </Form>
+                    </div>
                 </div>
-                <div className="grid grid-cols-12 gap-3">
-                    {assistants && assistants.length > 0 ? (
-                        assistants.map(
-                            ({ id, name, description, instructions }) => (
+                <div className="col-span-full p-4">
+                    <Heading as="h4" className="mb-4 text-center">
+                        Recent conversations
+                    </Heading>
+                    <div className="flex gap-4 overflow-x-auto">
+                        {threads.map((thread) => (
+                            <Link to={`${thread.assistant.oId}/${thread.id}`}>
                                 <Card
-                                    key={id}
-                                    className="col-span-full flex h-full flex-col flex-wrap justify-between gap-2 sm:col-span-6 lg:col-span-4 xl:col-span-3"
+                                    key={thread.id}
+                                    className="relative flex h-full min-w-[320px] flex-col justify-between overflow-hidden"
                                 >
-                                    <div>
-                                        <Heading as="h4">{name}</Heading>
-                                        {description && <p>{description}</p>}
-                                        {instructions && (
-                                            <p className="mb-4">
-                                                {ellipsisify(instructions)}
-                                            </p>
-                                        )}
-                                    </div>
-                                    <div className="flex gap-2 md:flex-wrap">
-                                        <threadFetcher.Form
-                                            method="DELETE"
-                                            action="/api/assistants"
-                                        >
-                                            <input
-                                                type="hidden"
-                                                name="assistantId"
-                                                value={id}
-                                            />
-                                            <Button
-                                                className="h-full"
-                                                size="sm"
-                                                type="submit"
-                                                color="danger"
-                                                iconBefore={getDeleteAssistantIcon(
-                                                    id,
-                                                )}
-                                            >
-                                                Delete
-                                            </Button>
-                                        </threadFetcher.Form>
-                                        <ButtonLink
-                                            to={`/${id}`}
-                                            size="sm"
-                                            variant="outline"
-                                            color="secondary"
-                                            iconBefore={
-                                                <ListIcon className="h-4 w-4" />
-                                            }
-                                        >
-                                            Details
-                                        </ButtonLink>
-                                        <ButtonLink
-                                            to={`${id}/create-file`}
-                                            size="sm"
-                                            variant="outline"
-                                            color="secondary"
-                                            iconBefore={
-                                                <FileIcon className="h-4 w-4" />
-                                            }
-                                        >
-                                            Add file
-                                        </ButtonLink>
-                                        <threadFetcher.Form
-                                            method="POST"
-                                            action={`${Paths.API_THREADS}?assistantId=${id}`}
-                                        >
-                                            <input
-                                                type="hidden"
-                                                name="assistantId"
-                                                value={id}
-                                            />
-                                            <Button
-                                                size="sm"
-                                                type="submit"
-                                                color="secondary"
-                                                iconBefore={getCreateThreadChatIcon(
-                                                    id,
-                                                )}
-                                            >
-                                                New thread
-                                            </Button>
-                                        </threadFetcher.Form>
-                                    </div>
+                                    <p>{thread.name}</p>
+                                    <p>{thread.assistant.name}</p>
+                                    {getThreadIsLoading(thread.id) && (
+                                        <div className="absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center bg-zinc-300/80 dark:bg-zinc-900/80">
+                                            <LoaderPinwheelIcon className="animate-spin" />
+                                        </div>
+                                    )}
                                 </Card>
-                            ),
-                        )
-                    ) : (
-                        <Banner
-                            icon={<FileQuestion className="h-4 w-4" />}
-                            className="min-w-[200px] basis-1/4"
-                            variant="warning"
-                        >
-                            No assistants available
-                        </Banner>
-                    )}
-                </div>
-                <HorizontalRule space="lg" />
-                <div className="mb-4 flex items-center gap-4">
-                    <Heading>Agents</Heading>
-                    <ButtonLink
-                        className="inline-flex items-center gap-2"
-                        to={Paths.CREATE_ASSISTANT}
-                        color="secondary"
-                        iconBefore={<PencilIcon className="h-4 w-4" />}
-                    >
-                        Create agent
-                    </ButtonLink>
-                </div>
-                <div className="grid grid-cols-12 gap-3">
-                    {agents && agents.length > 0 ? (
-                        <div className="col-span-full flex h-full flex-col flex-wrap justify-between gap-2 sm:col-span-6 lg:col-span-4 xl:col-span-3">
-                            <p>Placeholder...</p>
-                        </div>
-                    ) : (
-                        <Banner
-                            className="col-span-full sm:col-span-6 lg:col-span-4 xl:col-span-3"
-                            icon={<FileQuestion className="h-4 w-4" />}
-                            variant="warning"
-                        >
-                            No agents available
-                        </Banner>
-                    )}
-                </div>
-                <HorizontalRule space="lg" />
-                <div className="mb-4 flex items-center gap-4">
-                    <Heading>Threads</Heading>
-                </div>
-                <div className="grid gap-3 md:grid-cols-12">
-                    {threads && threads.length > 0 ? (
-                        threads.map((thread) => (
-                            <Card
-                                key={thread.id}
-                                className="col-span-1 flex h-full flex-col justify-between md:col-span-6 lg:col-span-4 xl:col-span-3"
-                            >
-                                <div>
-                                    <Heading as="h4">
-                                        {thread.name ?? "Untitled"}
-                                    </Heading>
-                                    <p className="mb-4 mt-2">
-                                        Chatting with:{" "}
-                                        <span className="inline-block rounded-xl bg-black px-2 py-1 text-white dark:bg-black dark:text-white">
-                                            {thread.assistant.name}
-                                        </span>
-                                    </p>
-                                </div>
-                                <div className="flex justify-end gap-2">
-                                    <deleteThreadFetcher.Form
-                                        method="DELETE"
-                                        action={`${Paths.API_THREADS}/${thread.id}`}
-                                    >
-                                        <Button
-                                            className="inline-flex items-center gap-2 self-end"
-                                            size="sm"
-                                            color="danger"
-                                            type="submit"
-                                            iconBefore={getDeleteChatIcon(
-                                                thread.id,
-                                            )}
-                                            disabled={
-                                                deleteThreadFetcher.state !==
-                                                "idle"
-                                            }
-                                        >
-                                            Delete chat
-                                        </Button>
-                                    </deleteThreadFetcher.Form>
-                                    <ButtonLink
-                                        className="inline-flex items-center gap-2 self-end"
-                                        to={`/dashboard/${thread.assistant.oId}/${thread.id}`}
-                                        size="sm"
-                                        color="secondary"
-                                        iconBefore={
-                                            navigation.state !== "idle" &&
-                                            navigation.location.pathname.includes(
-                                                thread.id,
-                                            ) ? (
-                                                <LoaderPinwheelIcon className="h-4 w-4 animate-spin" />
-                                            ) : (
-                                                <MessagesSquareIcon className="h-4 w-4" />
-                                            )
-                                        }
-                                    >
-                                        Open chat
-                                    </ButtonLink>
-                                </div>
-                            </Card>
-                        ))
-                    ) : (
-                        <Banner
-                            icon={<FileQuestion className="h-6 w-6" />}
-                            className="min-w-[200px] basis-1/4"
-                            variant="warning"
-                        >
-                            No threads available
-                        </Banner>
-                    )}
+                            </Link>
+                        ))}
+                    </div>
                 </div>
             </div>
             <Outlet />
