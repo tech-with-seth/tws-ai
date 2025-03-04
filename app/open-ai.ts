c1f24d365ee449830d7141d697e8b405bddddd8b
Seth Davis <sethdavis512@gmail.com>
2025-03-03 23:12:52 -0600

    Update env ref

--- app/open-ai.ts
+++ app/open-ai.ts
@@ -1,3 +1,3 @@
 import OpenAI from "openai";
 
-export const ai = new OpenAI();
+export const ai = new OpenAI({ apiKey: import.meta.env.VITE_OPENAI_API_KEY });
--- app/root.tsx
+++ app/root.tsx
@@ -36,6 +36,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
                 <Links />
             </head>
             <body className="h-full">
+                {JSON.stringify(import.meta.env)}
                 {children}
                 <ScrollRestoration />
                 <Scripts />
